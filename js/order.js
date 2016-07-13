9ad1bec834f5684ce12f0c9c1c963a10fd4ac5f0
nsokay <n.smirnovs.0612@gmail.com>
2016-07-13 11:03:10 +0300

    changes in forntend structure

--- js/order.js
+++ js/order.js
@@ -1,11 +1,3 @@
 function submit() {
     window.location = "submit.html"; // Redirecting to other page.
 }
-
-function loadUsers() {
-    //return fetch('./data/users.json')
-    return fetch('http://localhost:8080/students')
-        .then(function(response) {
-            return response.json();
-        });
-}
